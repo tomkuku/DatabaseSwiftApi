703e32a0a096169267bce37486504463529ef0d5
Tomasz Kukułka <tomasz.kukulka@ailleron.com>
2022-03-11 00:17:21 +0100

    Added docs to EntityAttributes

--- DatabaseApi/Config/EntityAttribute.swift
+++ DatabaseApi/Config/EntityAttribute.swift
@@ -24,6 +24,14 @@ class EntityAttribute {
 
 // MARK: - Attribute
 
+/**
+ Represents single attribute of entity which can't to be passed or can't returned nil value.
+ 
+ If name of property isn't equal to name of attribute in Model, you
+ can pass it's name using `init(_ key: String)`.
+ Using this wrapper in combination with optional attribute is
+ programmer error and can lead to unidentified behavior.
+ */
 @propertyWrapper
 final class Attribute<T>: EntityAttribute {
     var wrappedValue: T {
@@ -34,6 +42,12 @@ final class Attribute<T>: EntityAttribute {
 
 // MARK: - OptionalAttribute
 
+/**
+ Represents single attribute of entity which can to be passed an optional value.
+ 
+ If name of property isn't equal to name of attribute in Model, you
+ can pass it's name using `init(_ key: String)`.
+ */
 @propertyWrapper
 final class OptionalAttribute<T>: EntityAttribute {
     var wrappedValue: T? {
@@ -44,6 +58,12 @@ final class OptionalAttribute<T>: EntityAttribute {
 
 // MARK: - ToOneRelationship
 
+/**
+ Represents a ToOne relationship. This property must be an optional.
+ 
+ If name of property isn't equal to name of attribute in Model, you
+ can pass it's name using `init(_ key: String).`
+ */
 @propertyWrapper
 final class ToOneRelationship<T: EntityRepresentable>: EntityAttribute {
     var wrappedValue: T? {
@@ -60,6 +80,12 @@ final class ToOneRelationship<T: EntityRepresentable>: EntityAttribute {
 
 // MARK: - ToManyRelationship
 
+/**
+ Represents a ToMany relationship. This property must be `Set` of `EntityRepresentable`.
+ 
+ If name of property isn't equal to name of attribute in Model, you
+ can pass it's name using `init(_ key: String).`
+ */
 @propertyWrapper
 final class ToManyRelationship<T: EntityRepresentable>: EntityAttribute {
     var wrappedValue: Set<T> {
@@ -75,6 +101,7 @@ final class ToManyRelationship<T: EntityRepresentable>: EntityAttribute {
         }
         set {
             let set = databaseModelObject.mutableSetValue(forKey: key)
+            set.removeAllObjects()
             newValue.forEach {
                 set.add($0.managedObject)
             }
